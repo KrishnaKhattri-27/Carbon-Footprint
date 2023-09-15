1b6e04dd1640d82bcd0b1b514ac890eb41f6134f
Krishna <krishnakhattri24@gmail.com>
2023-09-16 05:01:50 +0530

    Folder structure updated

---
+++ src/data/Option.js
@@ -0,0 +1,50 @@
+const optionValue = {
+    food1: {
+      "Under 2 hours": 3,
+      "2-8 hours": 4,
+      "8-15 hours": 5,
+      "More than 15 hours": 7,
+    },
+    food2: {
+      train: 6,
+      "2-8 hours": 9,
+      "More than 15 hours": 1,
+    },
+    food3: {
+      "Under 2 hours": 1,
+      "2-8 hours": 3,
+      "8-15 hours": 5,
+    },
+    food4: {
+      "Under 2 hours": 7,
+      "2-8 hours": 8,
+      "8-15 hours": 9,
+      "More than 15 hours": 1,
+    },
+    travel1: {
+      "Under 2 hours": 7,
+      "2-8 hours": 8,
+      "8-15 hours": 9,
+      "More than 15 hours": 1,
+    },
+    travel2: {
+      "Under 2 hours": 7,
+      "2-8 hours": 8,
+      "8-15 hours": 9,
+      "More than 15 hours": 1,
+    },
+    travel3: {
+      "Under 2 hours": 7,
+      "2-8 hours": 8,
+      "8-15 hours": 9,
+      "More than 15 hours": 1,
+    },
+    travel4: {
+      "Under 2 hours": 7,
+      "2-8 hours": 8,
+      "8-15 hours": 9,
+      "More than 15 hours": 1,
+    },
+  };
+
+  export default optionValue;
---
+++ src/data/QuestionSet.js
@@ -0,0 +1,82 @@
+const questions = [
+    {
+      quest:
+        "How many hours a week do you spend in your car or on your motorbike for personal use including commuting?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "food1",
+    },
+    {
+      quest:
+        "How many hours a week do you spend on the train for personal use including commuting?",
+      options: ["train", "2-8 hours", "More than 15 hours"],
+      qID: "food2",
+    },
+    {
+      quest:
+        "How many hours a week do you spend on the bus for personal use including commuting?",
+      options: ["Under 2 hours", "2-8 hours", "8-15 hours"],
+      qID: "food3",
+    },
+    {
+      quest:
+        "In the last year, how many return flights have you made in total to the following locations?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "food4",
+    },
+    {
+      quest:
+        "In the last year, how many return flights have you made in total to the following locations?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "travel1",
+    },
+    {
+      quest:
+        "In the last year, how many return flights have you made in total to the following locations?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "travel2",
+    },
+    {
+      quest:
+        "In the last year, how many return flights have you made in total to the following locations?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "travel3",
+    },
+    {
+      quest:
+        "In the last year, how many return flights have you made in total to the following locations?",
+      options: [
+        "Under 2 hours",
+        "2-8 hours",
+        "8-15 hours",
+        "More than 15 hours",
+      ],
+      qID: "travel4",
+    },
+  ];
+
+  export default questions;
